7ee87b7a4e30925f30c5fd4a5bafb582fe8b8655
IuliiaNikitina1 <71014501+IuliiaNikitina1@users.noreply.github.com>
2024-04-26 20:00:46 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -60,44 +60,6 @@ import MarsovoField from './pages-parks/MarsovoField';
 import ParkCulture from './pages-parks/ParkCulture';
 import YMapsMap from '@pbe/react-yandex-maps/typings/Map';
 
-// async function initMap() {
-//     await ymaps3.ready;
-
-//     const {YMap, YMapDefaultSchemeLayer, multiRouter} = ymaps3;
-
-//     const map = new YMap(
-//         document.getElementById('map'),
-//         {
-//             location: {
-//                 center: [59.94, 30.3],
-//                 zoom: 12
-//             }
-//         }
-//     );
-
-//     const pointA = [59.939864, 30.314566];
-//     const pointB = [59.934345, 30.324611];
-
-//     let multiRoute = new multiRouter.MultiRoute({
-//         referencePoints: [
-//             pointA,
-//             pointB
-//         ],
-//         params: {
-//             //Тип маршрутизации - пешеходная маршрутизация.
-//             routingMode: 'pedestrian'
-//         }
-//         }, {
-//             // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
-//             boundsAutoApply: true
-//     });
-
-//     map.addChild(new YMapDefaultSchemeLayer());
-//     map.geoObjects.add(multiRoute);
-// }
-
-// initMap();
-
 const router = createBrowserRouter([
   {
     path: "/",
